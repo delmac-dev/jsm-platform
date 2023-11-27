7dc5f9b6c7d96e5428db1769c56f509ff791302a
delmac <delalitengue.dev@gmail.com>
2023-11-27 12:30:32 -0800

    done with searchform for now

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,8 +1,20 @@
+import SearchForm from '@/components/SearchForm'
 import React from 'react'
 
 const Page = () => {
   return (
-    <div>page</div>
+    <main className='flex-center paddings mx-auto w-full max-w-screen-2xl flex-col'>
+      <section className='nav-padding w-full'>
+        <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
+          <h1 className="sm:heading1 heading2 m-6 text-center text-white">
+             Javascript Mastery Resources
+          </h1>
+        </div>
+        <SearchForm />
+      </section>
+      Filters
+
+    </main>
   )
 }
 

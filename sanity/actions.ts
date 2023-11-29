ac9b16969fc22360c3a8e7324ce6d37074d2787d
delmac <delalitengue.dev@gmail.com>
2023-11-29 00:52:48 -0800

    created get resoureces function

--- sanity/actions.ts
+++ sanity/actions.ts
@@ -1,4 +1,6 @@
 import { groq } from 'next-sanity';
+import { readClient } from './lib/client';
+import { buildQuery } from './utils';
 
 interface GetResourcesParams {
     query: string;
@@ -8,5 +10,30 @@ interface GetResourcesParams {
 
 export const getResources = async (params: GetResourcesParams) => {
     const { query, category, page } = params;
+
+    try {
+        const resources = await readClient.fetch(
+            groq`${buildQuery({
+                type: 'resource',
+                query,
+                category,
+                page: parseInt(page)
+            })}{
+                title,
+                _id,
+                downloadLink,
+                "image": poster.asset->url,
+                views,
+                slug,
+                category
+            }`
+        );
+
+        return resources;
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
     
 }
